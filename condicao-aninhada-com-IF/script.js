10ee81b86334c266168cd694f53e75ba607aa6c2
claravitoria <limaclarinha17@gmail.com>
2026-03-25 10:37:40 -0300

    .\condicao-aninhada-com-IF\

--- condicao-aninhada-com-IF/script.js
+++ condicao-aninhada-com-IF/script.js
@@ -1,12 +1,12 @@
-let renda = Number (prompt("Digite sua renda:"));
-// ! símbolo de negação 
-// isNaN: is not number (verifica se NÃO é um número)
-if (isNaN(renda)){
-    if (renda >= 3000){
-        let nomeLimpo = prompt ("Nome limpo? (sim ou nao)");
-        if (nomeLimpo === "sim"){
-            alert("Financiamento aprovado");
-        }else {
+let renda = Number(prompt("digite sua renda"));
+// ! Símbolo de negação 
+// isNaN: Is not a number (verifica se NÃO é um número)
+if(!isNaN(renda)){
+    if (renda >= 3000) {
+        let nomelimpo = prompt ("Nome limpo?(sim ou não)");
+        if (nomelimpo === "sim") {
+            alert ("Financiamento aprovado");
+        } else {
             alert ("Nome negativado");
         }
     } else {
